20b4c4762ae9a6496b3b00374d7268994ba93059
est73 <brianross@outlook.com>
2021-02-24 00:28:25 -0800

    add welcome message

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import config
 import discord
+import random
 from discord.ext import commands
 
 initial_extensions = (
@@ -24,6 +25,28 @@ class RaidShack(commands.Bot):
         print(self.user.id)
         print('------')
         await self.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name="Pokémon Go"))
+
+    @staticmethod
+    async def on_member_join(member):
+        channel = member.guild.system_channel
+        welcome_text = [
+            "{0} joined the party.",
+            "{0} is here.",
+            "Welcome, {0}. We hope you brought pizza.",
+            "A wild {0} appeared.",
+            "{0} just landed.",
+            "{0} just slid into the server.",
+            "{0} just showed up!",
+            "Welcome {0}. Say hi!",
+            "{0} hopped into the server.",
+            "Everyone welcome {0}!",
+            "Glad you're here, {0}.",
+            "Good to see you, {0}.",
+            "Yay you made it, {0}!",
+        ]
+        await member.add_roles(member.guild.get_role(config.role_id))
+        if channel is not None:
+            await channel.send(random.choice(welcome_text).format(member.mention))
 
 
 intents = discord.Intents.default()
