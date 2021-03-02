6b19711542fa73ea8c246f3784713e2e907719d6
est73 <brianross@outlook.com>
2021-03-01 23:14:23 -0800

    update

--- cogs/routs.py
+++ cogs/routs.py
@@ -1,10 +1,11 @@
 from discord.ext import commands
 from quart import Quart, request, send_from_directory
-from quart_cors import route_cors
+from quart_cors import cors
 import config
 import discord
 
 app = Quart(__name__)
+app = cors(app, allow_origin="*")
 
 
 class Routs(commands.Cog):
@@ -19,7 +20,6 @@ class Routs(commands.Cog):
 
     @staticmethod
     @app.route('/', methods=['GET', 'POST'])
-    @route_cors(allow_origin="*")
     async def index():
         data = await request.get_json()
         print(data)
